f90142ebe2293558b9f3794799fda76ea62b64bc
luka <luka.sheylashvili@gmail.com>
2023-09-27 17:25:42 +0400

    first week push

---
+++ week1/main.py
@@ -0,0 +1,18 @@
+#1
+print(4-pow(2, 3)+(5*2)-(3/2))
+
+#2
+a = int(input("Number: "))
+print(bin(a))
+
+#3
+b = int(input("Hours: "))
+c = int(input("Salary: "))
+print(a*b)
+
+#4
+d = int(input("First"))
+e = int(input("Second"))
+f = int(input("Thrid"))
+
+print((d+e+f)/3)
